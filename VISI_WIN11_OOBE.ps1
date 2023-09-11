af925155c186ef377142bc650802daf393ebd509
wbilab <144415509+wbilab@users.noreply.github.com>
2023-09-11 07:14:25 +0200

    #

--- VISI_WIN11_OOBE.ps1
+++ VISI_WIN11_OOBE.ps1
@@ -2,72 +2,29 @@
 #   [PreOS] Update Module
 #================================================
 if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution for VM to 1600x"
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
     Set-DisRes 1600
 }
-else{
-    Write-Host  -ForegroundColor Green "Setting Display Resolution for VM to 1600x"
-    Set-DisRes 1900
-}
-
 
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module PowerShellGet -Force -SkipPublisherCheck
 Install-Module OSD -Force -SkipPublisherCheck
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force   
+Import-Module OSD -Force    
 
 #=======================================================================
-#   [OS] Params and Start-OSDCloudGUI
+#   [OS] Params and Start-OSDCloud
 #=======================================================================
-Write-Host -ForegroundColor Green "Create X:\OSDCloud\Automate\Start-OSDCloudGUI.json"
-$OSDCloudGUIParam = @'
-{
-    "BrandName":  "",
-    "OSActivation":  "Retail",
-    "OSEdition":  "Pro",
-    "OSLanguage":  "de-de",
-    "OSImageIndex":  9,
-    "OSName":  "Windows 11 22H2 x64",
-    "OSReleaseID":  "22H2",
-    "OSVersion":  "Windows 11",
-    "OSActivationValues":  [
-                                "Retail",
-                                "Volume"
-                            ],
-    "OSEditionValues":  [
-                            "Enterprise",
-                            "Pro"
-                        ],
-    "OSLanguageValues":  [
-                                "de-de",
-                                "fr-fr"
-                            ],
-    "OSNameValues":  [
-                            "Windows 11 22H2 x64",
-                            "Windows 10 22H2 x64"
-                        ],
-    "OSReleaseIDValues":  [
-                                "22H2"
-                            ],
-    "OSVersionValues":  [
-                            "Windows 11",
-                            "Windows 10"
-                        ],
-    "ClearDiskConfirm":  false,
-    "restartComputer":  true,
-    "updateDiskDrivers":  true,
-    "updateFirmware":  true,
-    "updateNetworkDrivers":  true,
-    "updateSCSIDrivers":  true
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "22H2"
+    OSEdition = "Pro"
+    OSLanguage = "de-de"
+    OSLicense = "Retail"
+    ZTI = $true
+    Firmware = $false
 }
-'@
-If (!(Test-Path "X:\OSDCloud\Automate")) {
-    New-Item "X:\OSDCloud\Automate" -ItemType Directory -Force | Out-Null
-}
-$OSDCloudGUIParam | Out-File -FilePath "X:\OSDCloud\Automate\Start-OSDCloudGUI.json" -Encoding ascii -Force
-
-Start-OSDCloudGUI
+Start-OSDCloud @Params
 
 #================================================
 #  [PostOS] OOBEDeploy Configuration
@@ -76,7 +33,7 @@ Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBED
 $OOBEDeployJson = @'
 {
     "AddNetFX3":  {
-                      "IsPresent":  false
+                      "IsPresent":  true
                   },
     "Autopilot":  {
                       "IsPresent":  false
@@ -91,6 +48,8 @@ $OOBEDeployJson = @'
                     "Microsoft.Messaging",
                     "Microsoft.MicrosoftOfficeHub",
                     "Microsoft.MicrosoftSolitaireCollection",
+                    "Microsoft.MicrosoftStickyNotes",
+                    "Microsoft.MSPaint",
                     "Microsoft.People",
                     "Microsoft.PowerAutomateDesktop",
                     "Microsoft.StorePurchaseApp",
@@ -108,7 +67,7 @@ $OOBEDeployJson = @'
                     "Microsoft.ZuneVideo"
                    ],
     "UpdateDrivers":  {
-                          "IsPresent":  false
+                          "IsPresent":  true
                       },
     "UpdateWindows":  {
                           "IsPresent":  false
@@ -123,29 +82,34 @@ $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeplo
 #================================================
 #  [PostOS] AutopilotOOBE Configuration Staging
 #================================================
+Write-Host -ForegroundColor Green "Define Computername:"
+$Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
+$TargetComputername = $Serial.Substring(4,3)
+
+$AssignedComputerName = "AkosCloud-$TargetComputername"
+Write-Host -ForegroundColor Red $AssignedComputerName
+Write-Host ""
 
 Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
 $AutopilotOOBEJson = @"
 {
-    "AssignedComputerName": "",
-    "AddToGroup":  "",
+    "AssignedComputerName" : "$AssignedComputerName",
+    "AddToGroup":  "AADGroupX",
     "Assign":  {
                    "IsPresent":  true
                },
-    "GroupTag":  "",
+    "GroupTag":  "GroupTagXXX",
     "Hidden":  [
-                   "AssignedComputerName",
                    "AddToGroup",
                    "AssignedUser",
                    "PostAction",
-                   "Run",
-                   "Docs",
+                   "GroupTag",
                    "Assign"
                ],
     "PostAction":  "Quit",
     "Run":  "NetworkingWireless",
     "Docs":  "https://google.com/",
-    "Title":  "Autopilot Registration"
+    "Title":  "Autopilot Manual Register"
 }
 "@
 
@@ -176,7 +140,7 @@ $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Fo
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
 powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
-#powershell.exe -Command "& {IEX (IRM oobetasks.osdcloud.ch)}"
+powershell.exe -Command "& {IEX (IRM oobetasks.osdcloud.ch)}"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
