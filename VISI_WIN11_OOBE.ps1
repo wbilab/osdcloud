b0fa895218b5bb153fbd8510e0ea5283ac7be42f
wbilab <144415509+wbilab@users.noreply.github.com>
2023-09-11 15:03:00 +0200

    #

--- VISI_WIN11_OOBE.ps1
+++ VISI_WIN11_OOBE.ps1
@@ -188,7 +188,7 @@ $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Fo
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
 powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
-powershell.exe -Command "& {IEX (IRM https://raw.githubusercontent.com/wbilab/osdcloud/main/oobetasks.ps1)}"
+powershell.exe -Command "& {IEX (IRM oobetasks.osdcloud.ch)}"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
