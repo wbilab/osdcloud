63fb9388519110031b789aef3061e8dd108ab5eb
wbilab <144415509+wbilab@users.noreply.github.com>
2023-09-17 20:15:42 +0200

    ok

--- CleanUp.ps1
+++ CleanUp.ps1
@@ -17,9 +17,6 @@ If (Test-Path -Path 'C:\Temp') {
     Get-ChildItem 'C:\Windows\Temp' -Filter *Events* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
 }
 
-# Remove TempAdmin
-Remove-LocalUser -Name "tempadmin"
-
 # Cleanup directories
 If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -Force }
 If (Test-Path -Path 'C:\WINDOWS\System32\GroupPolicy') { Remove-Item 'C:\WINDOWS\System32\GroupPolicy' -Recurse -Force }
