1412a407d77df46cf5b317f01586ce5652447dce
wbilab <144415509+wbilab@users.noreply.github.com>
2023-09-18 14:56:15 +0200

    ok

--- oobetasks-Win10.ps1
+++ oobetasks-Win10.ps1
@@ -28,11 +28,6 @@ Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.g
 #Write-Host -ForegroundColor DarkGray "Executing OOBEDeploy Script fomr OSDCloud Module"
 #Start-Process PowerShell -ArgumentList "-NoL -C Start-OOBEDeploy" -Wait
 
-if ([System.Environment]::OSVersion.Version -ge (New-Object Version "10.0.22000.0")) {
-write-Host -ForegroundColor DarkGray "Start Built-In Apps Cleanup"
-Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/Windows11CleanupBuiltinApps.ps1" -Wait
-}
-
 Write-Host -ForegroundColor DarkGray "Executing Cleanup Script"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/CleanUp.ps1" -Wait
 
---
+++ oobetasks.ps1
@@ -0,0 +1,8 @@
+if ([System.Environment]::OSVersion.Version -ge (New-Object Version "10.0.22000.0")) {
+write-Host -ForegroundColor DarkGray "Win11 OOBE"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/oobetasks-Win11.ps1" -Wait
+}
+else{
+write-Host -ForegroundColor DarkGray "Win10 OOBE"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/oobetasks-Win10.ps1" -Wait
+}
