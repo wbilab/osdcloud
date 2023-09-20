bb9ba78a1475f76ec70bc872bfaf817b861b8dca
wbilab <144415509+wbilab@users.noreply.github.com>
2023-09-20 11:43:53 +0200

    ok

--- oobetasks.ps1
+++ oobetasks.ps1
@@ -1,3 +1,8 @@
+Set-ExecutionPolicy Unrestricted -Force
+Install-Module OSD -Force -SkipPublisherCheck
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force  
+
 if ([System.Environment]::OSVersion.Version -ge (New-Object Version "10.0.22000.0")) {
 write-Host -ForegroundColor DarkGray "Win11"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/oobetasks-Win11.ps1" -Wait
