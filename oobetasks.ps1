8a54c290ebef67e9f771919c4d88bb1609316d6c
wbilab <144415509+wbilab@users.noreply.github.com>
2023-09-21 11:02:07 +0200

    ok

--- oobetasks.ps1
+++ oobetasks.ps1
@@ -17,9 +17,10 @@ $SendKeysScript = @"
 `# Definieren Sie den Download-URL fuer nircmd und den Zielpfad zum Speichern
 `$nircmdUrl = 'https://www.nirsoft.net/utils/nircmd-x64.zip'
 `$downloadPath = 'C:\OSDCloud\nircmd.zip'
-`
+
 # Zielverzeichnis fuer die extrahierten Dateien
 `$extractPath = 'C:\OSDCloud\nircmd'
+`$extractPathexe = 'C:\OSDCloud\nircmd\nircmd.exe'
 
 # Laden Sie nircmd herunter
 Invoke-WebRequest -Uri `$nircmdUrl -OutFile `$downloadPath
@@ -28,13 +29,13 @@ Invoke-WebRequest -Uri `$nircmdUrl -OutFile `$downloadPath
 Expand-Archive -Path `$downloadPath -DestinationPath `$extractPath -Force
 
 # Simulieren von 'Shift+F10'
-Start-Process -FilePath '`$extractPath\nircmd.exe' -ArgumentList 'sendkeypress Shift+F10' -Wait
+Start-Process -FilePath `$extractPathexe -ArgumentList 'sendkeypress Shift+F10' -Wait
 
 # Warten fuer 5 Sekunden
 Start-Sleep -Seconds 5
 
 # Simulieren von 'ALT+TAB'
-Start-Process -FilePath '`$extractPath\nircmd.exe' -ArgumentList 'sendkeypress Alt+Tab' -Wait
+Start-Process -FilePath `$extractPathexe -ArgumentList 'sendkeypress Alt+Tab' -Wait
 "@
 
 Out-File -FilePath $ScriptPathSendKeys -InputObject $SendKeysScript -Encoding ascii
