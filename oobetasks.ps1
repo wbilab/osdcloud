a751fd0cc428a8dcc44b71305a5b91210ba51268
wbilab <144415509+wbilab@users.noreply.github.com>
2025-08-06 10:54:48 +0200

    DisableBitLocker & Windows Updates

--- oobetasks.ps1
+++ oobetasks.ps1
@@ -101,6 +101,12 @@ write-Host -ForegroundColor DarkGray "Start Built-In Apps Cleanup"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/Windows11CleanupBuiltinApps.ps1" -Wait
 }
 
+Write-Host -ForegroundColor DarkGray "Disable Bitlocker Win11 24H2"
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\BitLocker /v PreventDeviceEncryption /t REG_DWORD /d 1 /f
+
+Write-Host -ForegroundColor DarkGray "Executing Windows & Driver Update Installer"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/AutoInstall-WindowsUpdates.ps1" -Wait
+
 Write-Host -ForegroundColor DarkGray "Executing Keyboard Language Skript"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/wbilab/osdcloud/main/Set-KeyboardLanguage.ps1" -Wait
 
